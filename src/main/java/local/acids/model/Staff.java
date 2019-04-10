685e41d67c722e25985e043419908ea80acd6e0e
Jonathan Theron <jonathant@webafrica.com>
2019-04-10 18:45:25 +0200

    2019-04-10

--- src/main/java/local/acids/model/Staff.java
+++ src/main/java/local/acids/model/Staff.java
@@ -5,10 +5,9 @@ public class Staff {
     private String firstName;
     private String lastName;
     private String emailAddress;
-    private boolean enabled;
+    private boolean staffAccountDisabled;
 
     private Staff(){
-
     }
 
     public String getFirstName() {
@@ -23,7 +22,45 @@ public class Staff {
         return emailAddress;
     }
 
-    public boolean isEnabled() {
-        return enabled;
+    public boolean isStaffAccountDisabled() {
+        return staffAccountDisabled;
+    }
+
+    private Staff(Builder builder){
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.emailAddress = builder.emailAddress;
+        this.staffAccountDisabled = builder.staffAccountDisabled;
+    }
+
+    public static class Builder{
+        private String firstName;
+        private String lastName;
+        private String emailAddress;
+        private boolean staffAccountDisabled;
+
+        public Builder firstName(String firstName){
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName){
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder emailAddress(String emailAddress){
+            this.emailAddress = emailAddress;
+            return this;
+        }
+
+        public Builder staffAccountDisabled(Boolean staffAccountDisabled){
+            this.staffAccountDisabled = staffAccountDisabled;
+            return this;
+        }
+
+        public Staff build(){
+            return new Staff(this);
+        }
     }
 }
